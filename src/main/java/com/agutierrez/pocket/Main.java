0da7cb039d264ccd36d782b2e64b0dc058dd6b8e
amaurs <amaury.gtz@gmail.com>
2016-03-03 16:29:50 -0600

    Added comments to make code more clear.

--- src/main/java/com/agutierrez/pocket/Main.java
+++ src/main/java/com/agutierrez/pocket/Main.java
@@ -1,13 +1,16 @@
 
 package com.agutierrez.pocket;
 
-public class Main
+public class Main 
 {
-	public static void main(String args[])
+	/**
+	 * Main entry point of the game, creates the engine object and starts the
+	 * game loop.
+	 */
+	public static void main(String args[]) 
 	{
 		PocketCube p = new PocketCube();
 
 		p.gameLoop();
 	}
 }
-
