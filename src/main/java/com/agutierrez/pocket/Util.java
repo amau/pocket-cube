b8e0ab3544cb05fcf1d99ab5a673aad53b183d59
amaurs <amaury.gtz@gmail.com>
2016-03-08 20:23:49 -0600

    Added functions to convert to and from factoradic using a String as input and output respectively.

--- src/main/java/com/agutierrez/pocket/Util.java
+++ src/main/java/com/agutierrez/pocket/Util.java
@@ -150,11 +150,135 @@ public class Util {
 		}
 		return result;
 	}
-	
-	public static int parseFactorial(String s)
+
+	/**
+	 * Turns the String representation of a factoradic number into its int
+	 * value.
+	 * 
+	 * @param s
+	 * @return
+	 * @throws Exception
+	 */
+	public static int parseFactorial(String s) throws Exception
 	{
-		return 0;
+        if (s == null) {
+            throw new NumberFormatException("null");
+        }
+
+        int result = 0;
+        int i = 0, len = s.length();
+        int digit;
+
+        if (len > 0) {
+            char lastChar = s.charAt(len - 1);
+            if (lastChar != '0') { 
+            	throw new NumberFormatException("All factoradic numbers must end in 0.");
+            }
+            
+            while (i < len - 1) {
+                digit = Character.digit(s.charAt(i++), 10);
+                if (digit < 0) {
+                	throw new NumberFormatException("Not valid character.");
+                } 
+                result += digit;
+                result *= (len - i);
+            }
+        } else {
+        	throw new NumberFormatException("Empty String.");
+        }
+        return result;
 	}
+	
+	/**
+	 * Converts the given decimal number into its factoradic representation. The
+	 * factoradic number system is used to induce a natural order to the
+	 * permutations of a given set. The resulting number is represented in a
+	 * string.
+	 * 
+	 * @param i
+	 *            Number to be converted.
+	 * @return The factoradic string representation of the given number.
+	 */
+	public static String toString(int i) {
+        int radix = 1;
+
+
+        char buf[] = new char[32];
+        int charPos = 31;
+
+
+        while (i >= radix) {
+            buf[charPos--] = digits[(i % radix)];
+            i = i / radix;
+            radix++;
+        }
+        buf[charPos] = digits[i];
+
+        return new String(buf, charPos, (32 - charPos));
+    }
+	
+	/**
+	 * Converts the given decimal number into its factoradic representation. The
+	 * factoradic number system is used to induce a natural order to the
+	 * permutations of a given set. The resulting number is represented in a
+	 * string. If the resulting string lenght is less than the given size
+	 * parameter, zeros are padded to the left to match the required size. Note
+	 * that zeros to the left wont affect a hypothetical conversion back to an
+	 * integer.
+	 * 
+	 * @param i
+	 *            Number to be converted.
+	 * @return The factoradic string representation of the given number.
+	 */
+	public static String toString(int i, int size) {
+		return padLeftZeros(toString(i), size);
+	}
+	
+	/**
+	 * Adds zeros to the left of the given string until its size matches the
+	 * required one.
+	 * 
+	 * @param s
+	 *            The string to be added with zeros.
+	 * @param size
+	 *            The desired size of the resulting string.
+	 * @return The given string with zeros padded to the left.
+	 */
+	public static String padLeftZeros(String s, int size)
+	{
+		return padLeft(s, size, '0');
+	}
+	
+	/**
+	 * Adds the given character to the left of the given string until its size
+	 * matches the required one.
+	 * 
+	 * @param s
+	 *            The string to be added with zeros.
+	 * @param size
+	 *            The desired size of the resulting string.
+	 * @param pad
+	 *            The char that will be added to the left.
+	 * @return The given string with the given character padded to the left.
+	 */
+	public static String padLeft(String s, int size, char pad) {
+		
+		if(s.length() > size)
+		{
+			return s;
+		}
+		
+		StringBuilder str = new StringBuilder("");
+		for(int j = 0; j < size; j++)
+		{
+			str.append(pad);
+		}
+		return str.toString().substring(s.length()) + s;
+	}
+	
+
+	
+
 	
 	/**
 	 * Convenient method to calculate powers of integer numbers..
@@ -226,4 +350,12 @@ public class Util {
 		}		
 		return result;
 	}
+	
+    /**
+     * All possible chars for representing a number as a String
+     */
+    final static char[] digits = {
+        '0' , '1' , '2' , '3' , '4' , '5' ,
+        '6' , '7' , '8' , '9'
+    };
 }
